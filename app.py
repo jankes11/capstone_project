73d3a666b428b4d8618bc8b31e7b98c4e117cd1f
Lukasz Jankowski <jankes11@o2.pl>
2023-04-25 11:16:43 +0100

    adding new files

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-#!/ursr/bin/env python
-import click
+from flask import Flask
+app = Flask(__name__)
 
-@click.command()
-def hello():
-    click.echo('hello world')
-    
+@app.route('/')
+def home():
+    return "<h1 style='text-align: center;'>Welcome to my Cloud DevOps Engineer Capstone Project!</h1>"
+
 if __name__ == '__main__':
-    hello()
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
